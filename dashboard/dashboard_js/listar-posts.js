82f21b3b8f5719fd73c07cfcb56696a5fe47b0f0
Duraxxi <alves.gian@ymail.com>
2025-11-03 04:54:43 -0300

    Botão de editar listar-conteudo funciona, correção footer css e outros detalhes dos html e css em geral

--- dashboard/dashboard_js/listar-posts.js
+++ dashboard/dashboard_js/listar-posts.js
@@ -14,7 +14,7 @@ async function loadConteudos(tipo = "") {
       <td>${c.id_conteudo}</td>
       <td>${c.co_titulo}</td>
       <td>${c.autor_nome || c.co_autor}</td>
-      <td>${c.tc_conteudo}</td>
+      <td>${c.co_tipo_conteudo}</td>
       <td>${c.co_data_inicio || ""}</td>
       <td class="actions">
         <button class="edit" onclick="editConteudo(${c.id_conteudo})">Editar</button>
@@ -24,6 +24,52 @@ async function loadConteudos(tipo = "") {
     tbody.appendChild(tr);
   });
 }
+
+let editingId = null;
+
+async function editConteudo(id){
+ console.log("Abrindo modal para ID:", id); // DEPURAÇÃO
+
+  editingId = id;
+
+  const res = await fetch(`${apiUrl}/${id}`);
+  const data = await res.json();
+
+  document.getElementById("editTitulo").value = data.co_titulo;
+  document.getElementById("editAutor").value = data.co_autor || data.autor_nome;
+  document.getElementById("editTipo").value = data.co_tipo_conteudo;
+  document.getElementById("editData").value = data.co_data_inicio ? data.co_data_inicio.split("T")[0] : "";
+
+  document.getElementById("editModal").style.display = "flex";
+}
+
+async function saveEdit() {
+  const body = {
+    co_titulo: document.getElementById("editTitulo").value,
+    co_autor: document.getElementById("editAutor").value,
+    co_tipo_conteudo: document.getElementById("editTipo").value,
+    co_data_inicio: document.getElementById("editData").value
+  };
+
+  const res = await fetch(`${apiUrl}/${editingId}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body)
+  });
+
+   if (res.ok) {
+    closeModal();
+    loadConteudos();
+  } else {
+    alert("Erro ao salvar");
+  }
+}
+
+function closeModal() {
+  document.getElementById("editModal").style.display = "none";
+}
+
+
 
 // EXCLUIR
 async function deleteConteudo(id) {
